cd50cb33e920b7f986ad92c05d085d9d702a16d9
cjoleary <cjoleary17@gmail.com>
2020-10-23 15:55:59 -0600

    function to generate modal overlay HTML markup, added click events to display and hide the modal overlay

--- js/app.js
+++ js/app.js
@@ -41,3 +41,47 @@
 
         gridContainer.innerHTML = employeeHTML;
     }
+
+    function displayModal(index) {
+        // use object destructuring make our template literal cleaner
+        let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+
+        let date = new Date(dob.date);
+
+        // create HTML markup for modal overlay
+        const modalHTML = `
+            <img class="avatar" src="${picture.large}" />
+            <div class="text-container">
+                <h2 class="name">${name.first} ${name.last}</h2>
+                <p class="email">${email}</p>
+                <p class="address">${city}</p>
+                <hr />
+                <p>${phone}</p>
+                <p class="address">${street}, ${state} ${postcode}</p>
+                <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+            </div>
+        `;
+
+        overlay.classList.remove("hidden");
+        modalContainer.innerHTML = modalHTML;
+    }
+
+// event listeners
+    // activates modal overlay when user clicks on an employee card element
+    gridContainer.addEventListener('click', (e) => {
+
+        // make sure the event target is not the gridContainer itself
+        if ( e.target !== gridContainer ) {
+
+            // select the card element based on its proximity to the element clicked
+            const card = e.target.closest('.card');
+            const index = card.getAttribute('data-index');
+
+            displayModal(index);
+        }
+    });
+
+    // hides modal overlay when user clicks X button
+    modalClose.addEventListener('click', () => {
+        overlay.classList.add("hidden");
+    });
