c17e5333c54a6b165a119ca100b57dfeed77e531
cjoleary <cjoleary17@gmail.com>
2020-10-26 16:08:59 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -119,23 +119,23 @@
     });
 
     // search bar to find employee by name  
-    const input = document.getElementById('search');
+    const search = document.getElementById('search');
 
     // search bar event listener
-    input.addEventListener('keyup', () => {
-        let search = input.value.toLowerCase();
+    search.addEventListener('keyup', () => {
+        let input = search.value.toLowerCase();
         let names = [];
         names = document.querySelectorAll('.name');
 
         for(let i = 0; i < names.length; i++){
             let nameText = names[i].innerText;
 
-            // if user types a letter that is included in an employee name, show those employees
-            if( nameText.toLowerCase().indexOf(search) > -1 ){
+            // if user input matches an employee name, show those employees
+            if( nameText.toLowerCase().indexOf(input) > -1 ){
                 let card = names[i].parentNode.parentNode;
                 card.style.display = '';
             } else {
-                // if user types a letter that is not included in an employee name, hide the employee
+                // if user input does not match an employee name, hide those employees
                 let card = names[i].parentNode.parentNode;
                 card.style.display = 'none';
             }
